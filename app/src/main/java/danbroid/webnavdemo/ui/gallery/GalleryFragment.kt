cbeb688a35a69c8dceb974659aed754cedd9f4ce
Dan Brough <dan@danbrough.org>
2019-08-27 07:01:27 +1200

    update

--- app/src/main/java/danbroid/webnavdemo/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/danbroid/webnavdemo/ui/gallery/GalleryFragment.kt
@@ -18,12 +18,12 @@ class GalleryFragment : Fragment() {
   private lateinit var galleryViewModel: GalleryViewModel
 
   override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      savedInstanceState: Bundle?
   ): View? {
     galleryViewModel =
-      ViewModelProviders.of(this).get(GalleryViewModel::class.java)
+        ViewModelProviders.of(this).get(GalleryViewModel::class.java)
     val root = inflater.inflate(R.layout.fragment_gallery, container, false)
     val textView: TextView = root.findViewById(R.id.text_gallery)
     galleryViewModel.text.observe(this, Observer {
